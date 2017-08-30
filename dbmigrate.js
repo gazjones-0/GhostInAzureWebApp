3fd980d05973683046c701b61a4441e3b273cca7
Gareth Jones <gazjones00@hotmail.com>
2017-08-30 16:51:03 +0100

    Added database migration script.

---
+++ dbmigrate.js
@@ -0,0 +1,20 @@
+// let users know which database is being initialised
+var logging = require('ghost-ignition').logging();
+var config = require('ghost/core/server/config');
+logging.info('Migrating ' + config.get('env'));
+
+// if we're running in Azure and not configuring production, warn the user
+if ((process.env.REGION_NAME) && (process.env.WEBSITE_SKU) && (!process.env.EMULATED) && (config.get('env') !== 'production')) {
+	logging.warn('Appear to be running in Azure but not configuring production database');
+};
+
+// migrate database to latest version
+var KnexMigrator = require('knex-migrator');
+knexMigrator = new KnexMigrator({
+    knexMigratorFilePath: __dirname + '\\node_modules\\ghost'
+});
+
+knexMigrator.migrate()
+	.catch(function onMigrateError(err) {
+		console.log('Migration failed: ' + err.message);
+	});
