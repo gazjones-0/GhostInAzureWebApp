3b647fc04056383f5d3ede36d6429df50f62f876
Gareth Jones <gazjones00@hotmail.com>
2017-11-30 17:40:58 +0000

    Added caching of statSync and lstatSync exceptions.

--- server.cache.stat.template.js
+++ server.cache.stat.template.js
@@ -1,21 +1,51 @@
 var fs = require('fs');
 fs.statCacheExists = false;
 fs.statCache = [];
+fs.statExceptionCache = [];
 
+// plug in our own version of lstatSync that caches the results
 var fslstatSync = fs.lstatSync;
 fs.lstatSync = function(path) {
-	var stats = fslstatSync(path);
-	if (!fs.statCache[path]) {
-		fs.statCache[path] = 'new fs.Stats(' + stats.dev + ', ' + stats.mode + ', ' + stats.nlink + ', ' + stats.uid + ', ' + stats.gid + ', ' + stats.rdev + ', ' + stats.blksize + ', ' + stats.ino + ', ' + stats.size + ', ' + stats.blocks + ', ' + stats.atime.getTime() + ', ' + stats.mtime.getTime() + ', ' + stats.ctime.getTime() + ', ' + stats.birthtime.getTime() + ');';
+	try {
+		var stats = fslstatSync(path);
+		if (!fs.statCache[path]) {
+			fs.statCache[path] = 'new fs.Stats(' + stats.dev + ', ' + stats.mode + ', ' + stats.nlink + ', ' + stats.uid + ', ' + stats.gid + ', ' + stats.rdev + ', ' + stats.blksize + ', ' + stats.ino + ', ' + stats.size + ', ' + stats.blocks + ', ' + stats.atime.getTime() + ', ' + stats.mtime.getTime() + ', ' + stats.ctime.getTime() + ', ' + stats.birthtime.getTime() + ');';
+		}
+		return stats;
+	} catch(ex) {
+		if (!fs.statExceptionCache[path]) {
+			fs.statExceptionCache[path] = '{ Error: new Error(\'' + convertStringToCode(ex.message) + '\'), errno: ' + ex.errno + ', code: \'' + convertStringToCode(ex.code) + '\', syscall: \'' + convertStringToCode(ex.syscall) + '\', path: \'' + convertStringToCode(ex.path) + '\' }';
+		}
+		throw ex;
 	}
-	return stats;
 }
 
+// plug in our own version of statSync that caches the results
 var fsstatSync = fs.statSync;
 fs.statSync = function(path) {
-	var stats = fsstatSync(path);
-	if (!fs.statCache[path]) {
-		fs.statCache[path] = 'new fs.Stats(' + stats.dev + ', ' + stats.mode + ', ' + stats.nlink + ', ' + stats.uid + ', ' + stats.gid + ', ' + stats.rdev + ', ' + stats.blksize + ', ' + stats.ino + ', ' + stats.size + ', ' + stats.blocks + ', ' + stats.atime.getTime() + ', ' + stats.mtime.getTime() + ', ' + stats.ctime.getTime() + ', ' + stats.birthtime.getTime() + ');';
+	try {
+		var stats = fsstatSync(path);
+		if (!fs.statCache[path]) {
+			fs.statCache[path] = 'new fs.Stats(' + stats.dev + ', ' + stats.mode + ', ' + stats.nlink + ', ' + stats.uid + ', ' + stats.gid + ', ' + stats.rdev + ', ' + stats.blksize + ', ' + stats.ino + ', ' + stats.size + ', ' + stats.blocks + ', ' + stats.atime.getTime() + ', ' + stats.mtime.getTime() + ', ' + stats.ctime.getTime() + ', ' + stats.birthtime.getTime() + ');';
+		}
+		return stats;
+	} catch(ex) {
+		if (!fs.statExceptionCache[path]) {
+			fs.statExceptionCache[path] = '{ Error: new Error(\'' + convertStringToCode(ex.message) + '\'), errno: ' + ex.errno + ', code: \'' + convertStringToCode(ex.code) + '\', syscall: \'' + convertStringToCode(ex.syscall) + '\', path: \'' + convertStringToCode(ex.path) + '\' }';
+		}
+		throw ex;
 	}
-	return stats;
 }
+
+// converts a string into an escaped javascript string
+function convertStringToCode(content) {
+	var strReplaceAll = require('str-replace-all');
+	content = strReplaceAll('\\', '\\\\', content);
+	content = strReplaceAll('\f', '\\f', content);
+	content = strReplaceAll('\n', '\\n', content);
+	content = strReplaceAll('\r', '\\r', content);
+	content = strReplaceAll('\t', '\\t', content);
+	content = strReplaceAll('\v', '\\v', content);
+	content = strReplaceAll('\'', '\\\'', content);
+	return content;
+};
